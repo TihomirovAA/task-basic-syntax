0f57212022beaad506289100c2a4fce9d4d421f8
aa.tihomirov <aa.tihomirov@tensor.ru>
2022-02-11 18:34:28 +0300

    задание по task-BasicSyntax

--- src/BasicSyntax.js
+++ src/BasicSyntax.js
@@ -1,8 +1,24 @@
+import { parse } from "path";
+
 export function romanToInteger(str) {
+    let dict = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000
+    };
     let result = 0;
-    /*
-    Напишите код, преобразующий число из римской записи в арабскую.
-    На вход вы получите строку, ответом должно быть число.
-     */
-    return result;
+    let digits = Object.keys(dict);
+    
+    for (var i = 0; i < str.length; i++) {
+        if (digits.indexOf(str[i]) < digits.indexOf(str[i+1])) {
+            result -= dict[str[i]];
+        } else {
+            result += dict[str[i]];
+        }
+      }
+    return  result;
 }
